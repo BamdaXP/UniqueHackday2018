1bb052bf59696860b5d98a21c6179cee70f92cdb
BamdaXP <wanxuanjun@sina.cn>
2018-06-02 22:18:16 +0800

    main

---
+++ Assets/Scripts/GamePlay/LineGenerator.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class LineGenerator : SerializedMonoBehaviour {
+    public class Point
+    {
+        public Vector3 pos;
+    }
+
+    public LineRenderer line;
+
+    public List<Point> points = new List<Point>();
+    [ReadOnly]
+    public List<Point> selectedPoints = new List<Point>();
+
+    [Range(0.01f,0.99f)]
+    public float connetSpeed = 0.2f;
+
+    public int index = 0;
+    public Point pointing { get { return points[index]; } }
+    public bool finished {
+        get
+        {
+            if (selectedPoints[selectedPoints.Count-1]!=points[points.Count-1])
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+    [ReadOnly]
+    public bool processing = false;
+
+    private void Start()
+    {
+        if (points.Count<=0)
+        {
+            Debug.LogWarning("No Point Candidates!!");
+            return;
+        }
+
+        selectedPoints.Clear();
+        //Add the first point to the 
+        selectedPoints.Add(points[0]);
+        line.positionCount = 1;
+        line.SetPosition(0, points[0].pos);
+    }
+
+    private void Update()
+    {}
+
+    private bool Connectable()
+    {
+        Point p = pointing;
+        if (p==null)
+        {
+            return false;
+        }
+        foreach (var point in selectedPoints)
+        {
+            if (p == point)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void Connect()
+    {
+        if (!Connectable()||processing)
+        {
+            return;
+        }
+        StopAllCoroutines();
+        line.SetPosition(line.positionCount - 1, selectedPoints[selectedPoints.Count - 1].pos);
+        selectedPoints.Add(pointing);
+        StartCoroutine(Connecting());
+    }
+
+    private IEnumerator Connecting()
+    {
+        processing = true;
+        line.positionCount += 1;
+        for (int i = 0; i <= 33; i++)
+        {
+            Vector3 v = Vector3.Lerp(line.GetPosition(line.positionCount - 2), selectedPoints[selectedPoints.Count - 1].pos, (float)i / 33);
+            line.SetPosition(line.positionCount - 1, v);
+            yield return null;
+        }
+        processing = false;
+    }
+
+    public void Cancel()
+    {
+        //Can not remove the begin
+        if (selectedPoints.Count<=1||processing)
+        {
+            return;
+        }
+        selectedPoints.RemoveAt(selectedPoints.Count - 1);
+        StopAllCoroutines();
+        StartCoroutine(Cancelling());
+    }
+    private IEnumerator Cancelling()
+    {
+        processing = true;
+        for (int i = 0; i <= 33; i++)
+        {
+            Vector3 v = Vector3.Lerp(line.GetPosition(line.positionCount - 1), selectedPoints[selectedPoints.Count - 1].pos, (float)i / 33);
+            line.SetPosition(line.positionCount - 1, v);
+            yield return null;
+        }
+        line.positionCount -= 1;
+        processing = false;
+    }
+
+
+}
+
---
+++ Assets/Scripts/System/AudioManager.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : Singleton<AudioManager> {
+    
+
+    [System.Serializable]
+	public class SE
+    {
+        
+        public string name = "New SE";
+        public AudioClip se;
+        [Range(0f, 3f)]
+        public float pitch = 1f;
+        [Range(0f, 1f)]
+        public float volume = 1f;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+    [System.Serializable]
+    public class BGM
+    {
+        
+        public string name = "New BGM";
+        public AudioClip bgm;
+        [Range(0f,3f)]
+        public float pitch = 1f;
+        [Range(0f, 1f)]
+        public float volume = 1f;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+
+    public List<SE> SEs;
+    public List<BGM> BGMs;
+    
+
+    private void Start()
+    {
+        foreach (var se in SEs)
+        {
+            se.source = gameObject.AddComponent<AudioSource>();
+            se.source.clip = se.se;
+            se.source.pitch = se.pitch;
+            se.source.volume = se.volume;
+        }
+        foreach (var bgm in BGMs)
+        { 
+            bgm.source = gameObject.AddComponent<AudioSource>();
+            bgm.source.clip = bgm.bgm;
+            bgm.source.pitch = bgm.pitch;
+            bgm.source.volume = bgm.volume;
+        }
+    }
+
+    public void PlaySE(string name)
+    {
+        foreach (var s in SEs)
+        {
+            if (s.name == name)
+            {
+                s.source.Play();
+            }
+        }
+    }
+    public void PlayBGM(string name ,bool loop=true)
+    {
+        foreach (var bgm in BGMs)
+        {
+            if (bgm.name == name)
+            {
+                bgm.source.loop = loop;
+                bgm.source.Play();
+            }
+        }
+    }
+    public void StopBGM(string name)
+    {
+        foreach (var s in SEs)
+        {
+            if (s.name == name)
+            {
+                s.source.Stop();
+                s.source.loop = false;
+            }
+        }
+    }
+
+    public void FadeInBGM(string name,int duration,bool loop = true)
+    {
+        foreach (var s in BGMs)
+        {
+            if (s.name == name)
+            {
+                StartCoroutine(FadeIn(s.source, duration));
+                s.source.loop = loop;
+                s.source.Play();
+            }
+        }
+    }
+
+    public void FadeOutBGM(string name, int duration)
+    {
+        foreach (var bgm in BGMs)
+        {
+            if (bgm.name == name)
+            {
+                StartCoroutine(FadeOut(bgm.source, duration));
+            }
+        }
+    }
+
+    IEnumerator FadeIn(AudioSource source,int duration)
+    {
+        //StopAllCoroutines();
+        float tmpVolume = 0f;
+        float fullVolume = source.volume;
+
+        for (float i = 0; i <= duration; i++)
+        {
+            tmpVolume = (i / duration) * fullVolume;
+            source.volume = tmpVolume;
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+
+    IEnumerator FadeOut(AudioSource source, int duration)
+    {
+        //StopAllCoroutines();
+        float tmpVolume = 0f;
+        float fullVolume = source.volume;
+        for (float i = duration; i >=0; i--)
+        {
+            tmpVolume = (i / duration) * fullVolume;
+            source.volume = tmpVolume;
+            yield return new WaitForEndOfFrame();
+        }
+        source.Stop();
+        source.loop = false;
+    }
+
+
+}
